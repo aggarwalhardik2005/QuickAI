58ba212b4438a442b98f0da46f63ea4e4f5fa8bb
aggarwalhardik2005 <hardikaggarwal01@gmail.com>
2026-05-18 15:39:04 +0530

    sidebar overlap fix

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = ({sidebar,setSidebar}) => {
     const isPremium = has?.({ plan: 'premium' });
   return (
     <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14
-        bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300
+        bottom-0 z-50 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300
         ease-in-out`}>
         <div className='my-7 w-full'>
             <img src={user.imageUrl} alt="User Avatar" className='w-13 rounded-full mx-auto'/>
